ebc9a71e96de25ebfaba99cd88104c8a671a2f7c
mustafa7ussien <mostafa.hussien.fci@gmail.com>
2023-09-17 17:19:45 +0300

    day1 lab python

---
+++ lab1.py
@@ -0,0 +1,55 @@
+# #Q1
+# bill = 47.28
+# tip = .15
+# totalBill = bill*tip/2
+# print(f"Each person needs to pay:  {totalBill} $")
+
+##############################################
+#Q2
+# x=input("Please Enter Number1")
+# y=input("Please Enter Number2")
+# result = eval(x)/eval(y)
+# print(f"result Divsion = {result}")
+
+########################
+# #Q3
+# word1 = "How"
+# word2 = "do"
+# word3 = "you"
+# word4 = "like"
+# word5 = "{}"
+# word6 = "so"
+# word7 = "far?"
+
+# print(f"{word1} {word2} {word3} {word4} {word5} {word6} {word7}")
+
+####################################
+
+# #Q4
+# word7="far?"
+
+# print(word7.replace('?','!'))
+# word5="{0}"
+# word5=word5.format("python")
+# print(word5)
+
+#print(word5)
+
+
+########################
+#Q5
+# statment=input("Please Enter statment: ")
+# print(len(statment))
+
+#################3
+#Q6
+# Full_name=input("Please Enter Full Name: ")
+# Age=input("Please Enter Age: ")
+# Faculty=input("Please Enter Faculty: ")
+# Birth_date=input("Please Enter Birth date: ")
+# Graduation_year=input("Please Enter Graduation year: ")
+
+
+# print(f"Your Data is  Full Name: {Full_name}\nAge: {Age}\nFaculty: {Faculty}\nBirth_date: {Birth_date}\nGraduation_year: {Graduation_year}  ")
+
+
